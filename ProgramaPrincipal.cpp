3c92370465366b63a147af7f7c7e69dc67cc8216
Qkessler <qkessler@gmail.com>
2019-04-06 15:52:10 +0200

    Programming full program, ResolucionDirecta incomplete.

--- ProgramaPrincipal.cpp
+++ ProgramaPrincipal.cpp
@@ -6,11 +6,37 @@
 
 using namespace std;
 
-void ResolucionDirecta(){
+class Solucion{
+  int NumConcatenaciones;
+  int Indice;
+}
+
+Solucion ResolucionDirecta(){
   
 }
 
+int Dividir(int p, int q){
+  return round((q-p)/2);
+}
 
+bool Pequeno(int p, int q, int base){
+  if ((q - p) <= base){
+    return true;
+  }
+  else return false;
+}
+
+void DivideVenceras(int p, int q, int base){
+  int k;
+  Solucion solucion;
+  if Pequeno(p, q, base){
+       solucion = ResolucionDirecta();
+  }
+  else {
+    k = Dividir(p, q, base);
+    solucion = Combinar(DivideVenceras(p, k-1, base), DivideVenceras(k, q));
+  }
+}
 
 int main(int argc, char *argv[]){
   
