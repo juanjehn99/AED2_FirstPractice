95c774394c3195c23d86bc31ae7ebf8e6a6afdd0
Qkessler <qkessler@gmail.com>
2019-04-06 19:27:57 +0200

    Fixing problems with arrays of words.

--- ProgramaPrincipal.cpp
+++ ProgramaPrincipal.cpp
@@ -16,8 +16,65 @@ class PalabrasGenerador(){
   string cadenaB;
 }
 
-Solucion ResolucionDirecta(){
-  
+Solucion ResolucionDirecta(int p, int q, int base, char A[], char B[]){
+  Solucion solucion;
+    int i, j;
+    bool anterior;
+    int contador, repeticion, inicio, maxi, indice, indicemax;
+    
+    contador = 0;
+    repeticion = 0;
+    maxi = 0;
+    anterior = false;
+    i = 0;
+    indicemax = 0;
+
+    while (i < (q-p)) {
+        j = 0;
+        contador = 0;
+        while ((A[i]!=B[j]) && (i < (q-p)) {
+            i++;
+            repeticion = 0;
+            anterior = false;
+        }
+        i++;
+        j++;
+        contador++;
+        while (contador<base) {
+            if (A[i]==B[j]) {
+                if (contador==base-1) {
+                    repeticion++;
+                    i++;
+                    j = 0;
+                    contador = 0;
+                    if (anterior == false) {
+                        indice = (i+1) - base;
+                    }
+                    anterior = true;
+                }
+                else {
+                    contador++;
+                    j++;
+                    i++;
+                }
+            }
+            else {
+                contador = m;
+            }
+        }
+        if (repeticion > maxi) {
+            maxi = repeticion;
+            repeticion = 0;
+            indicemax = indice;
+        }
+    }
+    inicio = indicemax;
+    solucion.NumConcatenaciones = maxi;
+    solucion.Indice = inicio;
+
+    return solucion;
+    
+    
 }
 
 int Dividir(int p, int q){
@@ -31,15 +88,15 @@ bool Pequeno(int p, int q, int base){
   else return false;
 }
 
-Solucion DivideVenceras(int p, int q, int base){
+ Solucion DivideVenceras(int p, int q, int base, char A[], char B[]){
   int k;
   Solucion solucion;
   if Pequeno(p, q, base){
-       solucion = ResolucionDirecta();
+      solucion = ResolucionDirecta(p, q, base, A[], B[]);
   }
   else {
     k = Dividir(p, q);
-    solucion = Combinar(DivideVenceras(p, k-1, base), DivideVenceras(k, q));
+    solucion = Combinar(DivideVenceras(p, k-1, base, A[], B[]), DivideVenceras(k, q, base, A[], B[]));
   }
 }
 
@@ -77,7 +134,8 @@ int main(int argc, char *argv[]){
   int p = 0;
   int q = strlen(PalabraGenerada.cadenaA);
   int base = strlen(PalabraGenerada.cadenaB);
-  Solucion SolucionProblema = DivideVenceras(p, q, base);
+  char A[q];
+  Solucion SolucionProblema = DivideVenceras(p, q, base, A[], B[]);
   cout << "El mayor num de concatenaciones de B en A es: " << SolucionProblema.NumConcatenaciones << endl;
   cout << "El indice en el que empieza es: " << SolucionProblema.Indice << endl;
 }
